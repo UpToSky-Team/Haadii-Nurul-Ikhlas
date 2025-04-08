df7fc253287d64253b7a3d8310da34b30ffb0b24
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2025-04-08 19:48:59 +0700

    Final Fiture Profile dan landing page

--- app/Livewire/Menu/Profile/StrukturYayasan.php
+++ app/Livewire/Menu/Profile/StrukturYayasan.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Menu\Profile;
 
+use App\Models\Struktur;
 use Livewire\Component;
 
 class StrukturYayasan extends Component
 {
     public function render()
     {
-        return view('livewire.menu.profile.struktur-yayasan');
+        $data = Struktur::orderBy('created_at', 'desc')->get();
+        return view('livewire.menu.profile.struktur-yayasan', [
+            'struktur' => $data, 
+        ]);
     }
 }
--- resources/views/livewire/menu/profile/struktur-yayasan.blade.php
+++ resources/views/livewire/menu/profile/struktur-yayasan.blade.php
@@ -1,68 +1,15 @@
 <body class="bg-gray-100 text-gray-800 mt-20 " >
     <div class="container mx-auto px-6 py-12 max-w-5xl pt-40">
-        <div class="max-w-4xl mx-auto bg-white p-6 shadow-lg rounded-lg mt-10 border-t-4 border-yellow-400">
-            <div class="flex items-center space-x-6">
-                <img src="/img/profile_picture/profile.jpg" alt="Dr. Murinto" class="w-32 h-32 rounded-full border-4 border-gray-300">
-                <div>
-                    <h2 class="text-lg font-bold text-blue-700">KEPALA YAYASAN</h2>
-                    <h1 class="text-2xl font-extrabold">Lorem ipsum dolor sit amet.</h1>
-                    <ul class="mt-2 text-gray-700">
-                        <li><strong>NIDN:</strong> 0510077302</li>
-                        <li><strong>NIY:</strong> 60040496</li>
-                        <li><strong>Jabatan Fungsional:</strong> Lektor</li>
-                        <li><strong>S1:</strong> FMIPA Matematika UNS Solo – Indonesia</li>
-                        <li><strong>S2:</strong> Ilmu Komputer UGM – Indonesia</li>
-                        <li><strong>S3:</strong> Ilmu Komputer UGM – Indonesia</li>
-                        <li><strong>Bidang Keahlian:</strong> Grafika Komputer, Pengolahan Citra, Computer Vision, Sistem Cerdas</li>
-                        <li><strong>Email:</strong> murintokusno[at]tif.uad.ac.id</li>
-                        <li><strong>Website:</strong> <a href="http://lorem.wordpress.com" class="text-blue-500 hover:underline">murintokusno.wordpress.com</a></li>
-                    </ul>
+        @foreach ($struktur as $data)
+            <div class="max-w-4xl mx-auto bg-white p-6 shadow-lg rounded-lg mt-10 border-t-4 border-yellow-400">
+                <div class="flex items-center space-x-6">
+                    <img src="{{ empty($data->gambar_url) ? '/img/profile_picture/profile.jpg' : Storage::url($data->gambar_url) }}" alt="{{ $data->nama }}" class="w-32 h-32 rounded-full border-4 border-gray-300">
+                    <div>
+                        <h2 class="text-lg font-bold text-blue-700">{{ Str::upper($data->jabatan) }}</h2>
+                        <h1 class="text-2xl font-extrabold">{{ Str::title($data->nama) }}</h1>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="max-w-4xl mx-auto bg-white p-6 shadow-lg rounded-lg mt-10 border-t-4 border-yellow-400">
-            <div class="flex items-center space-x-6">
-                <img src="/img/profile_picture/profile.jpg" alt="Dr. Murinto" class="w-32 h-32 rounded-full border-4 border-gray-300">
-                <div>
-                    <h2 class="text-lg font-bold text-blue-700">WAKIL KEPALA YAYASAN</h2>
-                    <h1 class="text-2xl font-extrabold">Lorem ipsum dolor sit amet.</h1>
-                    <ul class="mt-2 text-gray-700">
-                        <li><strong>NIDN:</strong> 0510077302</li>
-                        <li><strong>NIY:</strong> 60040496</li>
-                        <li><strong>Jabatan Fungsional:</strong> Lektor</li>
-                        <li><strong>S1:</strong> FMIPA Matematika UNS Solo – Indonesia</li>
-                        <li><strong>S2:</strong> Ilmu Komputer UGM – Indonesia</li>
-                        <li><strong>S3:</strong> Ilmu Komputer UGM – Indonesia</li>
-                        <li><strong>Bidang Keahlian:</strong> Grafika Komputer, Pengolahan Citra, Computer Vision, Sistem Cerdas</li>
-                        <li><strong>Email:</strong> murintokusno[at]tif.uad.ac.id</li>
-                        <li><strong>Website:</strong> <a href="http://lorem.wordpress.com" class="text-blue-500 hover:underline">murintokusno.wordpress.com</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-        <div class="max-w-4xl mx-auto bg-white p-6 shadow-lg rounded-lg mt-10 border-t-4 border-yellow-400">
-            <div class="flex items-center space-x-6">
-                <img src="/img/profile_picture/profile.jpg" alt="Dr. Murinto" class="w-32 h-32 rounded-full border-4 border-gray-300">
-                <div>
-                    <h2 class="text-lg font-bold text-blue-700">KEPALA SEKOLAH</h2>
-                    <h1 class="text-2xl font-extrabold">Lorem ipsum dolor sit amet.</h1>
-                    <ul class="mt-2 text-gray-700">
-                        <li><strong>NIDN:</strong> 0510077302</li>
-                        <li><strong>NIY:</strong> 60040496</li>
-                        <li><strong>Jabatan Fungsional:</strong> Lektor</li>
-                        <li><strong>S1:</strong> FMIPA Matematika UNS Solo – Indonesia</li>
-                        <li><strong>S2:</strong> Ilmu Komputer UGM – Indonesia</li>
-                        <li><strong>S3:</strong> Ilmu Komputer UGM – Indonesia</li>
-                        <li><strong>Bidang Keahlian:</strong> Grafika Komputer, Pengolahan Citra, Computer Vision, Sistem Cerdas</li>
-                        <li><strong>Email:</strong> murintokusno[at]tif.uad.ac.id</li>
-                        <li><strong>Website:</strong> <a href="http://lorem.wordpress.com" class="text-blue-500 hover:underline">murintokusno.wordpress.com</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-
+        @endforeach
     </div>
 </body>
