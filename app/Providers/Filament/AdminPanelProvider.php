9a5e7e7c0d8a0e6e403078705f44561de5353262
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2025-04-21 17:27:29 +0700

    Membuat dashboard & menampilkan data & Mengubah Tema admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Pages\Auth\LoginCustom;
+use App\Filament\Widgets\StatsDashboard;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -29,8 +30,14 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login(LoginCustom::class)
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#3B9B3C',
             ])
+            // ->favicon(asset('/img/logoyayasan.png'))
+            ->brandLogo('/img/logoyayasan.png')
+            // ->brandName('Yayasan Haadii Nurul Ikhlas - Admin')
+            ->spa()
+            ->sidebarFullyCollapsibleOnDesktop()
+            ->sidebarWidth('15  rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -39,7 +46,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+                // StatsDashboard::class,
             ])
             ->middleware([
                 EncryptCookies::class,
