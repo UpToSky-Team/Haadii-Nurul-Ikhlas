b4a53846039f4ae7028a4ce19e58cfaa79ae623d
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2025-04-24 00:53:29 +0700

    EDIT DIKIT

--- resources/views/livewire/menu/detail-berita.blade.php
+++ resources/views/livewire/menu/detail-berita.blade.php
@@ -1,86 +1,101 @@
-<body class="bg-gray-50 text-gray-800 font-sans">
+<body class="bg-gray-50 font-sans text-gray-800">
 
-    <div class="flex-grow pb-20 px-6 md:px-20 lg:px-32 pt-32">
+    <div class="flex-grow px-6 pb-20 pt-32 md:px-20 lg:px-32">
 
-      <!-- Breadcrumb -->
-      <nav class="flex px-5 py-3 border border-gray-300 rounded-lg bg-white text-black shadow-sm mb-10" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+        <!-- Breadcrumb -->
+        <nav class="mb-10 flex rounded-lg border border-gray-300 bg-white px-5 py-3 text-black shadow-sm"
+            aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
 
-          <!-- Item 1: Home -->
-          <li class="inline-flex items-center">
-            <a href="/" class="group inline-flex items-center text-sm font-medium text-black hover:text-green-500">
-              <svg class="w-3 h-3 me-2.5 text-black group-hover:text-green-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
-              </svg>
-              Home
-            </a>
-          </li>
+                <!-- Item 1: Home -->
+                <li class="inline-flex items-center">
+                    <a href="/"
+                        class="group inline-flex items-center text-sm font-medium text-black hover:text-green-500">
+                        <svg class="me-2.5 h-3 w-3 text-black group-hover:text-green-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                        </svg>
+                        Home
+                    </a>
+                </li>
 
-          <!-- Item 2: Publikasi -->
-          <li>
-            <div class="flex items-center">
-              <svg class="rtl:rotate-180 w-3 h-3 mx-1 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-              </svg>
-              <a href="/publikasi" class="ms-1 text-sm font-medium text-black hover:text-green-500 md:ms-2">Publikasi</a>
+                <!-- Item 2: Publikasi -->
+                <li>
+                    <div class="flex items-center">
+                        <svg class="mx-1 h-3 w-3 text-black rtl:rotate-180" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                        <a href="/publikasi"
+                            class="ms-1 text-sm font-medium text-black hover:text-green-500 md:ms-2">Publikasi</a>
+                    </div>
+                </li>
+
+                <!-- Item 3: Berita -->
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg class="mx-1 h-3 w-3 text-black rtl:rotate-180" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span class="ms-1 text-sm font-medium text-black">Berita</span>
+                    </div>
+                </li>
+
+            </ol>
+        </nav>
+
+        <div class="">
+
+            <!-- Judul Berita -->
+            <h1 class="mb-4 text-3xl font-bold text-gray-500 md:text-4xl">
+                {{ $berita?->judul }}
+            </h1>
+
+            <!-- Tanggal & Penulis -->
+            <div class="mb-6 text-sm text-gray-500">
+                Dipublikasikan pada <span class="font-medium">{{ $berita?->created_at->format('d F Y') }}</span> oleh
+                <span class="font-medium">{{ $berita->users->name }}</span>
             </div>
-          </li>
 
-          <!-- Item 3: Berita -->
-          <li aria-current="page">
-            <div class="flex items-center">
-              <svg class="rtl:rotate-180 w-3 h-3 mx-1 text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-              </svg>
-              <span class="ms-1 text-sm font-medium text-black">Berita</span>
+            <!-- Gambar -->
+            <img src="{{ Storage::url($berita?->slug) }}" alt="{{ $berita?->slug }}"
+                class="mb-6 w-full rounded-xl object-cover shadow" />
+
+            <!-- Isi Berita -->
+            <div
+                class="prose prose-p:leading-relaxed prose-img:rounded-xl prose-h2:mt-6 prose-h2:mb-2 prose-h2:text-x max-w-none">
+                <div class="text-justify">
+                    {!! $berita?->content !!}
+                </div>
             </div>
-          </li>
 
-        </ol>
-      </nav>
+            <div class="mt-4 flex flex-wrap gap-4">
+                @foreach ($berita?->gambarberitas as $gambarberita)
+                    <div class="w-1/3">
+                        <div class="group relative">
+                            <img src="{{ Storage::url($gambarberita->gambar_url) }}"
+                                alt="{{ $gambarberita->gambar_url }}" class="w-full rounded-xl object-cover shadow" />
+                            <div
+                                class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 transition-opacity group-hover:opacity-100">
+                                <p class="px-2 text-center text-sm text-white">{{ $gambarberita->caption }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
 
-      <div class="">
-
-    <!-- Judul Berita -->
-    <h1 class="text-3xl md:text-4xl font-bold mb-4">
-      {{ $berita?->judul }}
-    </h1>
-
-    <!-- Tanggal & Penulis -->
-    <div class="text-sm text-gray-500 mb-6">
-      Dipublikasikan pada <span class="font-medium">{{ $berita?->created_at->format('d F Y') }}</span> oleh <span class="font-medium">{{ $berita->users->name }}</span>
-    </div>
-
-    <!-- Gambar -->
-    <img src="{{ Storage::url($berita?->slug)}}" alt="{{$berita?->slug}}" class="rounded-xl shadow mb-6 w-full object-cover" />
-    
-    <!-- Isi Berita -->
-    <div class="prose max-w-none prose-p:leading-relaxed prose-img:rounded-xl prose-h2:mt-6 prose-h2:mb-2 prose-h2:text-x">
-      <div class="text-justify">
-        {!! $berita?->content !!}
-      </div>
-    </div>
-    
-    <div class="flex gap-4 flex-wrap mt-4">
-      @foreach ($berita?->gambarberitas as $gambarberita)
-      <div class="w-1/3">
-        <div class="relative group">
-          <img src="{{ Storage::url($gambarberita->gambar_url) }}" alt="{{ $gambarberita->gambar_url }}" class="rounded-xl shadow object-cover w-full" />
-          <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-          <p class="text-white text-sm px-2 text-center">{{ $gambarberita->caption }}</p>
-          </div>
+            <!-- Tombol Kembali -->
+            <div class="mt-10">
+                <a href="/publikasi"
+                    class="inline-block transform rounded-lg bg-blue-600 px-6 py-3 text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-700">
+                    ← Kembali ke Daftar Berita
+                </a>
+            </div>
         </div>
-      </div>
-      @endforeach
     </div>
 
-        <!-- Tombol Kembali -->
-        <div class="mt-10">
-          <a href="/publikasi" class="inline-block bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition ease-in-out duration-300 transform hover:scale-105">
-            ← Kembali ke Daftar Berita
-          </a>
-        </div>
-      </div>
-    </div>
-
-  </body>
+</body>
--- resources/views/livewire/menu/publikasi.blade.php
+++ resources/views/livewire/menu/publikasi.blade.php
@@ -1,139 +1,151 @@
 <body class="bg-gray-100 font-sans text-gray-800">
-    <div class="flex-grow pb-20 px-6 md:px-20 lg:px-32 pt-32">
+    <div class="flex-grow px-6 pb-20 pt-32 md:px-20 lg:px-32">
 
-      <!-- Breadcrumb -->
-      <nav class="flex px-5 py-3 border border-black/20 rounded-lg bg-white text-black shadow-sm mb-8" aria-label="Breadcrumb">
-        <ol class="inline-flex items-center space-x-2">
+        <!-- Breadcrumb -->
+        <nav class="mb-8 flex rounded-lg border border-black/20 bg-white px-5 py-3 text-black shadow-sm"
+            aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-2">
 
-          <!-- Item 1: Home -->
-          <li class="inline-flex items-center">
-            <a href="/" class="group inline-flex items-center text-sm font-medium text-gray-700 hover:text-green-500">
-              <svg class="w-4 h-4 mr-2 text-gray-700 group-hover:text-green-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-              </svg>
-              Home
-            </a>
-          </li>
+                <!-- Item 1: Home -->
+                <li class="inline-flex items-center">
+                    <a href="/"
+                        class="group inline-flex items-center text-sm font-medium text-gray-700 hover:text-green-500">
+                        <svg class="mr-2 h-4 w-4 text-gray-700 group-hover:text-green-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                        </svg>
+                        Home
+                    </a>
+                </li>
 
-          <!-- Item 2: Publikasi -->
-          <li>
-            <div class="flex items-center">
-              <svg class="w-3 h-3 mx-2 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-              </svg>
-              <a href="/publikasi" class="text-sm font-medium text-gray-700 hover:text-green-500">Publikasi</a>
+                <!-- Item 2: Publikasi -->
+                <li>
+                    <div class="flex items-center">
+                        <svg class="mx-2 h-3 w-3 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 9 4-4-4-4" />
+                        </svg>
+                        <a href="/publikasi"
+                            class="text-sm font-medium text-gray-700 hover:text-green-500">Publikasi</a>
+                    </div>
+                </li>
+
+            </ol>
+        </nav>
+
+        <!-- Konten -->
+        <div class="">
+
+            <!-- Judul -->
+            <h1 class="mb-8 text-start text-3xl font-bold text-gray-900 sm:text-4xl">Publikasi</h1>
+
+            <!-- Tabs -->
+            <div class="mb-10 flex gap-4">
+                <button onclick="showTab(event, 'berita')"
+                    class="tab-button rounded-full bg-blue-600 px-5 py-2 text-sm text-white hover:bg-blue-500">Berita</button>
+                <button onclick="showTab(event, 'artikel')"
+                    class="tab-button rounded-full bg-gray-200 px-5 py-2 text-sm text-gray-700 hover:bg-gray-300">Artikel</button>
+                <button onclick="showTab(event, 'dokumentasi')"
+                    class="tab-button rounded-full bg-gray-200 px-5 py-2 text-sm text-gray-700 hover:bg-gray-300">Dokumentasi</button>
             </div>
-          </li>
 
-        </ol>
-      </nav>
-
-      <!-- Konten -->
-      <div class="">
-
-        <!-- Judul -->
-        <h1 class="text-3xl sm:text-4xl font-bold mb-8 text-start text-gray-900">Publikasi</h1>
-
-        <!-- Tabs -->
-        <div class="flex gap-4 mb-10">
-          <button onclick="showTab(event, 'berita')" class="tab-button bg-blue-600 text-white px-5 py-2 rounded-full text-sm hover:bg-blue-500">Berita</button>
-          <button onclick="showTab(event, 'artikel')" class="tab-button bg-gray-200 text-gray-700 px-5 py-2 rounded-full text-sm hover:bg-gray-300">Artikel</button>
-          <button onclick="showTab(event, 'dokumentasi')" class="tab-button bg-gray-200 text-gray-700 px-5 py-2 rounded-full text-sm hover:bg-gray-300">Dokumentasi</button>
-        </div>
-
-        <!-- Tab Berita -->
-        <div id="berita" class="tab-content">
-            <h2 class="mb-4 text-xl font-semibold sm:text-2xl">Berita Pilihan</h2>
-            @foreach ($berita as $data)
-                <a href="{{ route('berita.detail', $data->id_berita) }}"
-                    class="flex flex-col rounded-xl bg-white shadow transition-transform duration-300 hover:scale-105 hover:shadow-md sm:flex-row">
-                    <div class="flex-shrink-0">
-                        <img src="{{ Storage::url($data->slug) }}" alt="{{ $data->slug }}"
-                            class="h-full w-full rounded-t-xl object-cover sm:h-full sm:w-80 sm:rounded-l-xl sm:rounded-t-none">
-                    </div>
-                    <div class="p-4 sm:m-5 sm:ml-4">
-                        <h3 class="mb-1 text-lg font-semibold">{{ $data->judul }}</h3>
-                        <div class="mb-1 text-gray-600">{!! Str::words($data->content, 25, '...') !!}</div>
-                        <p class="text-sm text-gray-500">Tanggal: {{ $data->created_at->format('d F Y') }}</p>
-                    </div>
-                </a>
-            @endforeach
-        </div>
-
-        <!-- Tab Artikel -->
-        <div id="artikel" class="tab-content hidden">
-            <h2 class="mb-4 text-xl font-semibold sm:text-2xl">Berita Pilihan</h2>
-            @foreach ($artikel as $data)
-                <a href="{{ route('artikel.detail', $data->id_artikel) }}"
-                    class="flex flex-col rounded-xl bg-white shadow transition-transform duration-300 hover:scale-105 hover:shadow-md sm:flex-row">
-                    <div class="flex-shrink-0">
-                        <img src="{{ Storage::url($data->slug) }}" alt="{{ $data->slug }}"
-                            class="h-full w-full rounded-t-xl object-cover sm:h-full sm:w-80 sm:rounded-l-xl sm:rounded-t-none">
-                    </div>
-                    <div class="p-4 sm:m-5 sm:ml-4">
-                        <h3 class="mb-1 text-lg font-semibold">{{ $data->judul }}</h3>
-                        <div class="mb-1 text-gray-600">{!! Str::words($data->content, 25, '...') !!}</div>
-                        <p class="text-sm text-gray-500">Oleh: {{$data->users->name}} | {{ $data->created_at->format('d F Y') }}</p>
-                    </div>
-                </a>
-            @endforeach
-        </div>
-
-        <!-- Tab Dokumentasi -->
-        <div id="dokumentasi" class="tab-content hidden">
-            <h2 class="mb-4 text-xl font-semibold sm:text-2xl">Galeri Dokumentasi</h2>
-            <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-                @if (!empty($foto))
-                    @foreach ($foto as $data)
-                        <a href="{{ Storage::url($data->galeri_url) }}" data-lightbox="galeri">
-                            <img src="{{ Storage::url($data->galeri_url) }}" alt="{{ $data->galeri_url }}"
-                                class="rounded-lg shadow transition-transform duration-300 hover:scale-105">
-                        </a>
-                    @endforeach
-                @endif
+            <!-- Tab Berita -->
+            <div id="berita" class="tab-content">
+                <h2 class="mb-4 text-xl font-semibold sm:text-2xl">Berita Pilihan</h2>
+                @foreach ($berita as $data)
+                    <a href="{{ route('berita.detail', $data->id_berita) }}"
+                        class="flex flex-col rounded-xl bg-white shadow transition-transform duration-300 hover:scale-105 hover:shadow-md sm:flex-row">
+                        <div class="flex-shrink-0">
+                            <img src="{{ Storage::url($data->slug) }}" alt="{{ $data->slug }}"
+                                class="h-full w-full rounded-t-xl object-cover sm:h-full sm:w-80 sm:rounded-l-xl sm:rounded-t-none">
+                        </div>
+                        <div class="p-4 sm:m-5 sm:ml-4">
+                            <h3 class="mb-1 text-lg font-semibold">{{ $data->judul }}</h3>
+                            <div class="mb-1 text-gray-600">{!! Str::words($data->content, 25, '...') !!}</div>
+                            <p class="text-sm text-gray-500">Tanggal: {{ $data->created_at->format('d F Y') }}</p>
+                        </div>
+                    </a>
+                @endforeach
             </div>
-            <div class="mt-10 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
-                @if (!empty($video))
-                    @foreach ($video as $data)
-                        <a href="{{ Storage::url($data->galeri_url) }}" data-lightbox="galeri">
-                            <div class="relative overflow-hidden rounded-lg shadow-md">
-                              <iframe class="h-64 w-full md:h-96"
-                                src="{{ Storage::url($data->galeri_url) }}" title="Video Kegiatan Yayasan"
-                                frameborder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowfullscreen>
-                              </iframe>
-                              <div class="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-50"></div>
-                            </div>
-                        </a>
-                    @endforeach
-                @endif
+
+            <!-- Tab Artikel -->
+            <div id="artikel" class="tab-content hidden">
+                <h2 class="mb-4 text-xl font-semibold sm:text-2xl">Berita Pilihan</h2>
+                @foreach ($artikel as $data)
+                    <a href="{{ route('artikel.detail', $data->id_artikel) }}"
+                        class="flex flex-col rounded-xl bg-white shadow transition-transform duration-300 hover:scale-105 hover:shadow-md sm:flex-row">
+                        <div class="flex-shrink-0">
+                            <img src="{{ Storage::url($data->slug) }}" alt="{{ $data->slug }}"
+                                class="h-full w-full rounded-t-xl object-cover sm:h-full sm:w-80 sm:rounded-l-xl sm:rounded-t-none">
+                        </div>
+                        <div class="p-4 sm:m-5 sm:ml-4">
+                            <h3 class="mb-1 text-lg font-semibold">{{ $data->judul }}</h3>
+                            <div class="mb-1 text-gray-600">{!! Str::words($data->content, 25, '...') !!}</div>
+                            <p class="text-sm text-gray-500">Oleh: {{ $data->users->name }} |
+                                {{ $data->created_at->format('d F Y') }}</p>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
+
+            <!-- Tab Dokumentasi -->
+            <div id="dokumentasi" class="tab-content hidden">
+                <h2 class="mb-4 text-xl font-semibold sm:text-2xl">Galeri Dokumentasi</h2>
+                <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+                    @if (!empty($foto))
+                        @foreach ($foto as $data)
+                            <a href="{{ Storage::url($data->galeri_url) }}" data-lightbox="galeri">
+                                <img src="{{ Storage::url($data->galeri_url) }}" alt="{{ $data->galeri_url }}"
+                                    class="rounded-lg shadow transition-transform duration-300 hover:scale-105">
+                            </a>
+                        @endforeach
+                    @endif
+                </div>
+                <div class="mt-10 grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+                    @if (!empty($video))
+                        @foreach ($video as $data)
+                            <a href="{{ Storage::url($data->galeri_url) }}" data-lightbox="galeri">
+                                <div class="relative aspect-video w-full overflow-hidden rounded-lg shadow-md">
+                                    <iframe class="h-full w-full" src="{{ Storage::url($data->galeri_url) }}"
+                                        title="Video Kegiatan Yayasan" frameborder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                        allowfullscreen>
+                                    </iframe>
+                                    <div
+                                        class="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-50">
+                                    </div>
+                                </div>
+                            </a>
+                        @endforeach
+                    @endif
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Script Tabs -->
-    <script>
-        function showTab(event, tabId) {
-            const tabs = document.querySelectorAll('.tab-content');
-            const buttons = document.querySelectorAll('.tab-button');
+        <!-- Script Tabs -->
+        <script>
+            function showTab(event, tabId) {
+                const tabs = document.querySelectorAll('.tab-content');
+                const buttons = document.querySelectorAll('.tab-button');
 
-            buttons.forEach(btn => {
-                btn.classList.remove('bg-blue-500', 'text-white');
-                btn.classList.add('bg-gray-200', 'text-gray-800');
-            });
+                buttons.forEach(btn => {
+                    btn.classList.remove('bg-blue-500', 'text-white');
+                    btn.classList.add('bg-gray-200', 'text-gray-800');
+                });
 
-            event.currentTarget.classList.remove('bg-gray-200', 'text-gray-800');
-            event.currentTarget.classList.add('bg-blue-500', 'text-white');
+                event.currentTarget.classList.remove('bg-gray-200', 'text-gray-800');
+                event.currentTarget.classList.add('bg-blue-500', 'text-white');
 
-            tabs.forEach(tab => {
-                if (tab.id === tabId) {
-                    tab.classList.remove('hidden');
-                } else {
-                    tab.classList.add('hidden');
-                }
-            });
-        }
-    </script>
+                tabs.forEach(tab => {
+                    if (tab.id === tabId) {
+                        tab.classList.remove('hidden');
+                    } else {
+                        tab.classList.add('hidden');
+                    }
+                });
+            }
+        </script>
 
-  </body>
+</body>
